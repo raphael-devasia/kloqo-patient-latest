c53b5ba3b7835315ffe8a5bc73e143b1e3c8ee0a
Juner Gypsy <junergypsy@gmail.com>
2025-09-29 15:55:39 +0000

    Change the title to My schedule  (_for element h1_)

--- src/app/appointments/page.tsx
+++ src/app/appointments/page.tsx
@@ -7,7 +7,7 @@ export default function AppointmentsPage() {
     <div className="relative h-full">
       <div className="space-y-6">
         <div>
-          <h1 className="text-2xl font-bold tracking-tight md:text-3xl">Your Schedule</h1>
+          <h1 className="text-2xl font-bold tracking-tight md:text-3xl">My Schedule</h1>
           <p className="text-muted-foreground">
             A timeline of your upcoming appointments.
           </p>
