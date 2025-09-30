a87649245ed7a3b1ce26abf6ef232d36ae997e88
Juner Gypsy <junergypsy@gmail.com>
2025-09-30 11:07:11 +0000

    Move down (_for element h1_)

--- src/app/appointments/page.tsx
+++ src/app/appointments/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function AppointmentsPage() {
   return (
     <div className="relative h-full">
-      <div className="space-y-6">
+      <div className="space-y-6 pt-4">
         <div>
           <h1 className="text-2xl font-bold tracking-tight md:text-3xl">My Schedule</h1>
         </div>
