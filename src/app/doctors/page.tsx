bc72f0caf53ce64de09b8321d1b74dc7c9b63aac
Juner Gypsy <junergypsy@gmail.com>
2025-09-30 10:16:43 +0000

    Move it down (_for element h1_)

--- src/app/doctors/page.tsx
+++ src/app/doctors/page.tsx
@@ -14,7 +14,7 @@ export default function DoctorsPage() {
 
   return (
     <div className="space-y-4">
-      <div className="flex items-center relative justify-center">
+      <div className="flex items-center relative justify-center mt-4">
         <button onClick={() => router.back()} className="absolute left-0">
           <ChevronLeft className="w-6 h-6" />
         </button>
