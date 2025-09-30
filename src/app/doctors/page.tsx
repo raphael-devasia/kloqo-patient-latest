3cb81193b76331fb8c1f1876f12e469343ca1af3
Juner Gypsy <junergypsy@gmail.com>
2025-09-30 11:04:28 +0000

    Change the heading to Clinic / Doctors (_for element h1_)

--- src/app/doctors/page.tsx
+++ src/app/doctors/page.tsx
@@ -10,7 +10,7 @@ export default function DoctorsPage() {
   const searchParams = useSearchParams();
   const specialty = searchParams.get('specialty');
 
-  const pageTitle = specialty ? specialty : "Clinics";
+  const pageTitle = specialty ? specialty : "Clinic / Doctors";
 
   return (
     <div className="space-y-4">
